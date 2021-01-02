ec5e43d2878803f53f737a551b8d5d83076557e0
AJ Adams <ajaoviedo@gmail.com>
2021-01-02 12:56:29 -0500

    Initial commit

---
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,257 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.Site = new System.Windows.Forms.TextBox();
+            this.Username = new System.Windows.Forms.TextBox();
+            this.Password = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button1.Location = new System.Drawing.Point(547, 86);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(160, 53);
+            this.button1.TabIndex = 0;
+            this.button1.TabStop = false;
+            this.button1.Text = "Read Table";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button2.Location = new System.Drawing.Point(173, 467);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(160, 53);
+            this.button2.TabIndex = 1;
+            this.button2.TabStop = false;
+            this.button2.Text = "Insert Row";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button3.Location = new System.Drawing.Point(784, 86);
+            this.button3.Margin = new System.Windows.Forms.Padding(4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(160, 53);
+            this.button3.TabIndex = 2;
+            this.button3.TabStop = false;
+            this.button3.Text = "Delete";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(285, 78);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(172, 66);
+            this.button4.TabIndex = 7;
+            this.button4.TabStop = false;
+            this.button4.Text = "Retrieve All Tables";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(46, 78);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(172, 66);
+            this.button5.TabIndex = 8;
+            this.button5.TabStop = false;
+            this.button5.Text = "Retrieve All Databases";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.HorizontalScrollbar = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(62, 173);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(143, 164);
+            this.listBox1.TabIndex = 9;
+            this.listBox1.TabStop = false;
+            // 
+            // listBox2
+            // 
+            this.listBox2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.listBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.HorizontalScrollbar = true;
+            this.listBox2.ItemHeight = 20;
+            this.listBox2.Location = new System.Drawing.Point(300, 173);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(143, 164);
+            this.listBox2.TabIndex = 10;
+            this.listBox2.TabStop = false;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.Location = new System.Drawing.Point(500, 173);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(496, 270);
+            this.richTextBox1.TabIndex = 11;
+            this.richTextBox1.TabStop = false;
+            this.richTextBox1.Text = "";
+            // 
+            // Site
+            // 
+            this.Site.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.Site.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Site.Location = new System.Drawing.Point(52, 416);
+            this.Site.Name = "Site";
+            this.Site.Size = new System.Drawing.Size(131, 27);
+            this.Site.TabIndex = 12;
+            this.Site.TabStop = false;
+            // 
+            // Username
+            // 
+            this.Username.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.Username.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Username.Location = new System.Drawing.Point(189, 416);
+            this.Username.Name = "Username";
+            this.Username.Size = new System.Drawing.Size(131, 27);
+            this.Username.TabIndex = 13;
+            this.Username.TabStop = false;
+            // 
+            // Password
+            // 
+            this.Password.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.Password.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Password.Location = new System.Drawing.Point(326, 416);
+            this.Password.Name = "Password";
+            this.Password.Size = new System.Drawing.Size(131, 27);
+            this.Password.TabIndex = 14;
+            this.Password.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(97, 380);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 20);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "Site";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(215, 380);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 20);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "Username";
+            // 
+            // label3
+            // 
+            this.label3.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(351, 380);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(83, 20);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "Password";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Password);
+            this.Controls.Add(this.Username);
+            this.Controls.Add(this.Site);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "AJ";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox Site;
+        private System.Windows.Forms.TextBox Username;
+        private System.Windows.Forms.TextBox Password;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string connetionString;
+            SqlConnection cnn;
+            string ds = @"DESKTOP-B4R6OUU\SQLEXPRESS";
+            string db = listBox1.GetItemText(listBox1.SelectedItem);
+            connetionString = $@"Data Source={ds};Initial Catalog={db};integrated Security=SSPI";
+            using (cnn = new SqlConnection(connetionString)) //idisposable
+            {
+                try
+                {
+                    cnn.Open();
+                    SqlCommand cmd;
+                    SqlDataReader dr;
+                    String sql, outp = "";
+                    String tablename = listBox2.GetItemText(listBox2.SelectedItem); //get selected table
+
+                    if(tablename == "")
+                    {
+                        MessageBox.Show("Select a table");
+                        return;
+                    }
+                    sql = $"Select * from {tablename}"; //query to get all columns from table
+                    cmd = new SqlCommand(sql, cnn);
+                    dr = cmd.ExecuteReader();
+                    int i = 1;
+                    while (dr.Read())
+                    {
+                        outp += i+ ". Site: " + dr.GetValue(0) + " Username: " + dr.GetValue(1) + " Password: " + dr.GetValue(2) + "\n";
+                        i++;
+                    }
+                    String output = $"Table: {tablename}\n\n" + outp;
+                    richTextBox1.Text = output;
+                    cnn.Close();
+                }
+                catch (SqlException ex)
+                {
+                    MessageBox.Show($"Cannot open connection! ErrorCode: {ex.ErrorCode} Error: {ex.Message}");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Cannot open connection! Error: {ex.Message}");
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string connetionString;
+            SqlConnection cnn;
+            string ds = @"DESKTOP-B4R6OUU\SQLEXPRESS";
+            string db = listBox1.GetItemText(listBox1.SelectedItem);
+            connetionString = $@"Data Source={ds};Initial Catalog={db};integrated Security=SSPI";
+            using (cnn = new SqlConnection(connetionString))
+            {
+                try
+                {
+                    cnn.Open();
+                    SqlCommand cmd;
+                    String sql = "";
+                    String tablename = listBox2.GetItemText(listBox2.SelectedItem); //get selected table
+                    String site = Site.Text;
+                    String user = Username.Text;
+                    String pass = Password.Text;
+                    if(site == "" || user == "" || pass == "")
+                    {
+                        MessageBox.Show("Fill in all fields");
+                        return;
+                    }
+                    sql = $"Insert into {tablename} values('{site}','{user}','{pass}');"; //query to add row to table
+                    cmd = new SqlCommand(sql, cnn);
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show($"Succesfully added row to {tablename}");
+                    cnn.Close();
+                }
+                catch (SqlException ex)
+                {
+                    MessageBox.Show($"Cannot open connection! ErrorCode: {ex.ErrorCode} Error: {ex.Message}");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Cannot open connection! Error: {ex.Message}");
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string connetionString;
+            SqlConnection cnn;
+            string ds = @"DESKTOP-B4R6OUU\SQLEXPRESS";
+            string db = listBox1.GetItemText(listBox1.SelectedItem);
+            connetionString = $@"Data Source={ds};Initial Catalog={db};integrated Security=SSPI";
+            using (cnn = new SqlConnection(connetionString))
+            {
+                try
+                {
+                    cnn.Open();
+                    SqlCommand cmd;
+                    String sql = "";
+                    String tablename = listBox2.GetItemText(listBox2.SelectedItem); //get selected table
+
+                    if (tablename == "")
+                    {
+                        MessageBox.Show("Select a table");
+                        return;
+                    }
+                    sql = $"Delete from {tablename};"; //query to delete all values from table
+                    cmd = new SqlCommand(sql, cnn);
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show($"Succesfully deleted all values from {tablename}");
+                    cnn.Close();
+                }
+                catch (SqlException ex)
+                {
+                    MessageBox.Show($"Cannot open connection! ErrorCode: {ex.ErrorCode} Error: {ex.Message}");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Cannot open connection! Error: {ex.Message}");
+                }
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string connetionString;
+            SqlConnection cnn;
+            string ds = @"DESKTOP-B4R6OUU\SQLEXPRESS";
+            string db = listBox1.GetItemText(listBox1.SelectedItem);
+            connetionString = $@"Data Source={ds};Initial Catalog={db};integrated Security=SSPI";
+            using (cnn = new SqlConnection(connetionString))
+            {
+                try
+                {
+                    cnn.Open();
+                    SqlCommand cmd;
+                    SqlDataReader dr;
+                    String sql = "";
+
+                    if(db == "")
+                    {
+                        MessageBox.Show("Select a database");
+                        return;
+                    }
+                    sql = $"USE {db}; SELECT * FROM sys.Tables;"; //query to see all tables in database
+                    cmd = new SqlCommand(sql, cnn);
+                    dr = cmd.ExecuteReader();
+                    listBox2.Items.Clear(); //clear previous items from listbox
+                    while (dr.Read())
+                    {
+                        listBox2.Items.Add(dr.GetValue(0)); //add each table
+                    }
+                }
+                catch (SqlException ex)
+                {
+                    MessageBox.Show($"Cannot open connection! ErrorCode: {ex.ErrorCode} Error: {ex.Message}");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Cannot open connection! Error: {ex.Message}");
+                }
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            string connetionString;
+            SqlConnection cnn;
+            string ds = @"DESKTOP-B4R6OUU\SQLEXPRESS";
+            connetionString = $@"Data Source={ds};integrated Security=SSPI"; //use data source to get list of databases
+            using (cnn = new SqlConnection(connetionString))
+            {
+                try
+                {
+                    cnn.Open();
+                    SqlCommand cmd;
+                    SqlDataReader dr;
+                    String sql = "";
+
+                    sql = $"SELECT name FROM sys.databases;"; //query to see all databases in the server
+                    cmd = new SqlCommand(sql, cnn);
+                    dr = cmd.ExecuteReader();
+                    listBox1.Items.Clear(); //clear previous items from listbox
+                    while (dr.Read())
+                    {
+                        listBox1.Items.Add(dr.GetValue(0)); //add each database
+                    }
+                }
+                catch (SqlException ex)
+                {
+                    MessageBox.Show($"Cannot open connection! ErrorCode: {ex.ErrorCode} Error: {ex.Message}");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Cannot open connection! Error: {ex.Message}");
+                }
+            }
+        }
+    }
+}
